fc836d39efe3af876333df45e8c6df5709007505
ryutaro yamaguchi <yamaguchi@tugbase.co.jp>
2024-09-06 15:19:44 +0900

    src/App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders den-test1 Camera PWA heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/den-test1 Camera PWA/i);
+  expect(headingElement).toBeInTheDocument();
 });
